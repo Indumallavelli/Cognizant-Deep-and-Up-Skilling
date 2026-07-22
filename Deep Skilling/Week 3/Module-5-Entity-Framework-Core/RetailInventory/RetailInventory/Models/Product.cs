e344d7ffadc14c8429bf2ba4f2fb787601157593
Indu Mallavelli <indumallavelli05@gmail.com>
2026-07-22 19:10:37 +0530

    Module 5 - Lab 8 completed

--- Deep Skilling/Week 3/Module-5-Entity-Framework-Core/RetailInventory/RetailInventory/Models/Product.cs
+++ Deep Skilling/Week 3/Module-5-Entity-Framework-Core/RetailInventory/RetailInventory/Models/Product.cs
@@ -8,6 +8,8 @@
 
         public decimal Price { get; set; }
 
+        public int StockQuantity { get; set; }
+
         public int CategoryId { get; set; }
 
         public Category Category { get; set; } = null!;

bbf6cb495e03b1fc5cb4b6fdb20dfd81cb96682c
Indu Mallavelli <indumallavelli05@gmail.com>
2026-07-22 19:41:50 +0530

    Module 5 - Lab 9 completed

--- Deep Skilling/Week 3/Module-5-Entity-Framework-Core/RetailInventory/RetailInventory/Data/AppDbContext.cs
+++ Deep Skilling/Week 3/Module-5-Entity-Framework-Core/RetailInventory/RetailInventory/Data/AppDbContext.cs
@@ -14,5 +14,32 @@ namespace RetailInventory.Data
             optionsBuilder.UseSqlServer(
                 @"Server=localhost\SQLEXPRESS;Database=RetailInventoryDB;Trusted_Connection=True;TrustServerCertificate=True");
         }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Electronics" },
+                new Category { Id = 2, Name = "Groceries" }
+            );
+
+            modelBuilder.Entity<Product>().HasData(
+                new Product
+                {
+                    Id = 1,
+                    Name = "Smartphone",
+                    Price = 25000,
+                    CategoryId = 1,
+                    StockQuantity = 50
+                },
+                new Product
+                {
+                    Id = 2,
+                    Name = "Wheat Flour",
+                    Price = 800,
+                    CategoryId = 2,
+                    StockQuantity = 100
+                }
+            );
+        }
     }
 }

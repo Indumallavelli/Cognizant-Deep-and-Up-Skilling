a41873a68a06c8d633c43ccbb4c93e93375b9f75
Indu Mallavelli <indumallavelli05@gmail.com>
2026-07-22 18:51:48 +0530

    Module 5 - Labs 1 to 3 completed

---
+++ Deep Skilling/Week 3/Module-5-Entity-Framework-Core/RetailInventory/RetailInventory/Program.cs
@@ -0,0 +1,36 @@
+﻿using RetailInventory.Data;
+using RetailInventory.Models;
+
+using var context = new AppDbContext();
+
+// Add a category
+var category = new Category
+{
+    Name = "Electronics"
+};
+
+context.Categories.Add(category);
+context.SaveChanges();
+
+// Add a product
+var product = new Product
+{
+    Name = "Laptop",
+    Price = 65000,
+    CategoryId = category.Id
+};
+
+context.Products.Add(product);
+context.SaveChanges();
+
+Console.WriteLine("Data inserted successfully!");
+
+// Display all products
+var products = context.Products.ToList();
+
+Console.WriteLine("\nProducts:");
+
+foreach (var p in products)
+{
+    Console.WriteLine($"{p.Id}  {p.Name}  ₹{p.Price}");
+}
